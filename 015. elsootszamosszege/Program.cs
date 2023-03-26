4cde44d9b961dbe194fdb7e30fc3cde926f69d8e
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 14:41:25 +0200

    Az első 5 szám összege

---
+++ 015. elsootszamosszege/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015.elsootszamosszege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int osszeadelsoot = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                osszeadelsoot = osszeadelsoot + i;
+            }
+            Console.WriteLine("Az első öt szám összege: " + osszeadelsoot + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
